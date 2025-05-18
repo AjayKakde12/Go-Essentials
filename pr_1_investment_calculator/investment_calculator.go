99b5803eda4bf78a4792ebc06ba314bb0a5687f1
Ajay Kakde <kakdeajay12@yahoo.com>
2025-05-18 16:56:16 +0530

    modifications: updates regarding accepting user inputs using fmt.Scan

--- pr_1_investment_calculator/investment_calculator.go
+++ pr_1_investment_calculator/investment_calculator.go
@@ -6,13 +6,25 @@ import (
 )
 
 func main() {
-	const inflationRate float64 = 6
-	var investmentAmount float64 = 1000
-	expectedReturnRate := 5.5
-	var years float64 = 10
+	// const inflationRate float64 = 6
+	// var investmentAmount float64 = 1000
+	// expectedReturnRate := 5.5
+	// var years float64 = 10
 
 	// var investmentAmount, years float64 = 1000, 10
 	// investmentAmount, years, expectedReturnRate := 1000, 10, 5.5
+
+	const inflationRate float64 = 6
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Println("Enter investment amount:")
+	fmt.Scan(&investmentAmount)
+	fmt.Println("Enter expected return rate:")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Println("Enter number of years:")
+	fmt.Scan(&years)
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	var futureRealValue = futureValue / math.Pow(1+inflationRate/100, years)
