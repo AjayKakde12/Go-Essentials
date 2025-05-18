e111aee4717a1c07e5b6ec9af15eb549c1246019
Ajay Kakde <kakdeajay12@yahoo.com>
2025-05-18 20:47:03 +0530

    Implementation error handling and file handeling

--- control_structures/bank.go
+++ control_structures/bank.go
@@ -1,9 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const BALANCE_FILE string = "balance.txt"
+
+func writeBalanceToFile(balance float64) {
+	balancetext := fmt.Sprint(balance)
+	os.WriteFile(BALANCE_FILE, []byte(balancetext), 0644)
+}
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(BALANCE_FILE)
+	if err != nil {
+		writeBalanceToFile(0)
+		return 0, errors.New("storage not found")
+	}
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+	if err != nil {
+		return 1000, errors.New("storage contains invalid data")
+	}
+	return balance, nil
+}
 
 func main() {
-	var accountBalance float64 = 1000
+	var accountBalance, err = getBalanceFromFile()
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("-----------------------------")
+	}
 	var choice int
 
 	for {
@@ -19,7 +50,7 @@ func main() {
 		fmt.Printf("Selected: %v\n", choice)
 
 		if choice == 1 {
-			fmt.Printf("Your account balance is %.2f", accountBalance)
+			fmt.Printf("Your account balance is %.2f\n", accountBalance)
 		} else if choice == 2 {
 			var depositeAmount float64
 			fmt.Print("Your deposit: ")
@@ -30,6 +61,7 @@ func main() {
 			}
 			accountBalance += depositeAmount
 			fmt.Println("Balance updated: ", accountBalance)
+			writeBalanceToFile(accountBalance)
 		} else if choice == 3 {
 			var withdrawMoney float64
 			fmt.Print("Your withdraw amount: ")
@@ -40,6 +72,7 @@ func main() {
 			}
 			accountBalance -= withdrawMoney
 			fmt.Println("Balance updated: ", accountBalance)
+			writeBalanceToFile(accountBalance)
 		} else {
 			fmt.Println("Goodbye!")
 			break
