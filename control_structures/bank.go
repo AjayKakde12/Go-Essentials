82eb7d6f1d2f107ad29cc0a97335efe766e756c9
Ajay Kakde <kakdeajay12@yahoo.com>
2025-05-18 20:03:12 +0530

    Control Statements if-else

---
+++ control_structures/bank.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	var accountBalance float64 = 1000
+	var choice int
+
+	fmt.Println("Welcome to Go Bank")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+	fmt.Printf("Selected: %v\n", choice)
+
+	if choice == 1 {
+		fmt.Printf("Your account balance is %.2f", accountBalance)
+	} else if choice == 2 {
+		var depositeAmount float64
+		fmt.Print("Your deposit: ")
+		fmt.Scan(&depositeAmount)
+		if depositeAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0")
+			return
+		}
+		accountBalance += depositeAmount
+		fmt.Println("Balance updated: ", accountBalance)
+	} else if choice == 3 {
+		var withdrawMoney float64
+		fmt.Print("Your withdraw amount: ")
+		fmt.Scan(&withdrawMoney)
+		if withdrawMoney > accountBalance || withdrawMoney <= 0 {
+			fmt.Println("Invalid amount. Must be less than account balancd and greater than 0")
+			return
+		}
+		accountBalance -= withdrawMoney
+		fmt.Println("Balance updated: ", accountBalance)
+	} else {
+		fmt.Println("Goodbye!")
+	}
+}
---
+++ control_structures/go.mod
@@ -0,0 +1,3 @@
+module github.com/AjayKakde12/Go-Essentials/pr3-bank-example
+
+go 1.24.3
