a3835c7447f04c9cd9e4f1bb8ac552cfa21e8d05
Ajay Kakde <kakdeajay12@yahoo.com>
2025-05-24 18:15:38 +0530

    [Fn Mutation]: Structs Mutation (Changing Values)

--- understanding_structs/structs.go
+++ understanding_structs/structs.go
@@ -16,19 +16,9 @@ func (user User) outputUserDetails() {
 	fmt.Println(user.firstName, user.lastName, user.birthdate)
 }
 
-func main() {
-
-	appUser := User{
-		firstName: getUserData("Please enter your first name: "),
-		lastName:  getUserData("Please enter your last name: "),
-		birthdate: getUserData("Please enter your birthdate (MM/DD/YYYY): "),
-		createdAt: time.Now(),
-	}
-
-	// ... do something awesome with that gathered data!
-
-	appUser.outputUserDetails()
-	outputUserDetailsUsingPointer(&appUser)
+func (user *User) clearUserName() {
+	(*user).firstName = ""
+	(*user).lastName = ""
 }
 
 func outputUserDetailsUsingPointer(user *User) {
@@ -40,4 +30,21 @@ func getUserData(promptText string) string {
 	var value string
 	fmt.Scan(&value)
 	return value
+}
+
+func main() {
+	appUser := User{
+		firstName: getUserData("Please enter your first name: "),
+		lastName:  getUserData("Please enter your last name: "),
+		birthdate: getUserData("Please enter your birthdate (MM/DD/YYYY): "),
+		createdAt: time.Now(),
+	}
+
+	// ... do something awesome with that gathered data!
+
+	appUser.outputUserDetails()
+	outputUserDetailsUsingPointer(&appUser)
+
+	appUser.clearUserName()
+	appUser.outputUserDetails()
 }
