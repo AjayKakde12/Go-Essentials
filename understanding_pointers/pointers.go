695e5851ea7d5cce59996c99fbfa68fcbf02aca0
Ajay Kakde <kakdeajay12@yahoo.com>
2025-05-24 17:29:47 +0530

    solved warnings

--- understanding_pointers/pointers.go
+++ understanding_pointers/pointers.go
@@ -5,8 +5,7 @@ import "fmt"
 func main() {
 	age := 32
 
-	var agePointer *int
-	agePointer = &age
+	agePointer := &age
 
 	fmt.Println("Age pointer: ", *agePointer)
 	adultYears := getAdultYears(agePointer)
@@ -20,7 +19,7 @@ func main() {
 }
 
 func getAdultYears(age *int) int {
-	fmt.Println("Age: ", age, &age, *&age)
+	fmt.Println("Age: ", age, &age)
 	return *age - 18
 }
 
